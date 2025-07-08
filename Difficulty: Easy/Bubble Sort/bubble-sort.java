ae7525812a7dc4167b53289ae8a9e73aac56bfb0
utkarsh <123744256+um26@users.noreply.github.com>
2025-07-08 09:04:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.java
+++ Difficulty: Easy/Bubble Sort/bubble-sort.java
@@ -4,13 +4,16 @@ class Solution {
         // code here
         int n=arr.length;
         for(int i=0;i<n-1;i++){
+            boolean swap=false;
             for(int j=0;j<n-1-i;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swap=true;
                 }
             }
+            if(!swap) break;
         }
     }
 }
